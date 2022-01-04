c8a43d008b3609ebe93cb6b31a4592fe5b77fb98
Vanny Yeh <70726508+vannyyeh@users.noreply.github.com>
2022-01-04 10:15:54 -0800

    ReactPracticeUpdate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 //Create a react app from scratch.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render(What to SharedWorker, Where to show it)
+
 //It should display a h1 heading.
+//ReactDOM.render(<h1>My Fav Foods</h1>, document.getElementById("root"));
+
 //It should display an unordered list (bullet points).
+ReactDOM.render(
+  <div>
+    <h1>My Fav Drink</h1>
+    <ul>
+      <li>Fruit Tea</li>
+      <li>Bubble Tea</li>
+      <li>Earl Grey</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should contain 3 list elements.
